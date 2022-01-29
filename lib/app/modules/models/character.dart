c0df166371989f70708619c5f34afd95b3a5bb28
ronie <roneylf@gmail.com>
2022-01-29 11:42:00 -0300

    Adicionado data methods em Models

--- lib/app/modules/models/character.dart
+++ lib/app/modules/models/character.dart
@@ -1,11 +1,63 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
 
 class Character {
   int id;
-
+  int apiId;
   String name;
   Character({
     required this.id,
+    required this.apiId,
     required this.name,
   });
+
+  Character copyWith({
+    int? id,
+    int? apiId,
+    String? name,
+  }) {
+    return Character(
+      id: id ?? this.id,
+      apiId: apiId ?? this.apiId,
+      name: name ?? this.name,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'apiId': apiId,
+      'name': name,
+    };
+  }
+
+  factory Character.fromMap(Map<String, dynamic> map) {
+    return Character(
+      id: map['id']?.toInt() ?? 0,
+      apiId: map['apiId']?.toInt() ?? 0,
+      name: map['name'] ?? '',
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Character.fromJson(String source) =>
+      Character.fromMap(json.decode(source));
+
+  @override
+  String toString() => 'Character(id: $id, apiId: $apiId, name: $name)';
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is Character &&
+        other.id == id &&
+        other.apiId == apiId &&
+        other.name == name;
+  }
+
+  @override
+  int get hashCode => id.hashCode ^ apiId.hashCode ^ name.hashCode;
 }
--- lib/app/modules/models/film.dart
+++ lib/app/modules/models/film.dart
@@ -1,9 +1,60 @@
+import 'dart:convert';
+
 class Film {
   int id;
-
+  int apiId;
   String title;
   Film({
     required this.id,
+    required this.apiId,
     required this.title,
   });
+
+  Film copyWith({
+    int? id,
+    int? apiId,
+    String? title,
+  }) {
+    return Film(
+      id: id ?? this.id,
+      apiId: apiId ?? this.apiId,
+      title: title ?? this.title,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'apiId': apiId,
+      'title': title,
+    };
+  }
+
+  factory Film.fromMap(Map<String, dynamic> map) {
+    return Film(
+      id: map['id']?.toInt() ?? 0,
+      apiId: map['apiId']?.toInt() ?? 0,
+      title: map['title'] ?? '',
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Film.fromJson(String source) => Film.fromMap(json.decode(source));
+
+  @override
+  String toString() => 'Film(id: $id, apiId: $apiId, title: $title)';
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is Film &&
+        other.id == id &&
+        other.apiId == apiId &&
+        other.title == title;
+  }
+
+  @override
+  int get hashCode => id.hashCode ^ apiId.hashCode ^ title.hashCode;
 }
