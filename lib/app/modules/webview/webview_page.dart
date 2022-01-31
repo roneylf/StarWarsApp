b38d948d60402f5fa7373cf38b6c4ce376b22db6
ronie <roneylf@gmail.com>
2022-01-31 13:45:43 -0300

    Criado Splash Screen

--- lib/app/modules/webview/webview_page.dart
+++ lib/app/modules/webview/webview_page.dart
@@ -9,10 +9,49 @@ class WebviewPage extends StatefulWidget {
 }
 
 class WebviewPageState extends State<WebviewPage> {
+  bool _isLoading = true;
+  // ignore: unused_field
+  late InAppWebViewController _webViewController;
   @override
   Widget build(BuildContext context) {
-    return InAppWebView(
-      initialUrlRequest: URLRequest(url: Uri.parse("https://flutter.dev")),
+    return SafeArea(
+      child: Stack(
+        children: [
+          InAppWebView(
+            initialUrlRequest:
+                URLRequest(url: Uri.parse("https://starwars.com/community")),
+            onWebViewCreated: (InAppWebViewController controller) {
+              _webViewController = controller;
+            },
+            onLoadStart: (controller, url) {
+              setState(() {
+                _isLoading = true;
+              });
+            },
+            onLoadStop: (controller, url) async {
+              setState(
+                () {
+                  _isLoading = false;
+                },
+              );
+            },
+            onLoadError: (controller, url, number, description) {
+              setState(() {
+                _isLoading = false;
+              });
+            },
+          ),
+          if (_isLoading)
+            const Center(
+              child: CircularProgressIndicator(),
+            ),
+        ],
+      ),
     );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
   }
 }
